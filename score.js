58afe06593d0b42de287b25a0f8f6e7b008541ef
Yuvashree R <91588494+YuvashreeRchan@users.noreply.github.com>
2024-03-08 02:02:59 +0530

    Update score.js

--- score.js
+++ score.js
@@ -1,13 +1,8 @@
 // Iteration 5: Store the player score and display it on the game over screen
-let score = localStorage.getItem("score");
-console.log(score);
-
 const playAgainBtn = document.getElementById("play-again-button");
 const scoreBoard = document.getElementById("score-board");
 
-scoreBoard.innerHTML = score;
-
-playAgainBtn.onclick = (evt) => 
-{
-    location.href = "./game.html";
-};
+scoreBoard.textContent= localStorage.getItem("score");
+playAgainBtn.addEventListener("click", () => {
+    window.location.href = "./game.html";
+});
